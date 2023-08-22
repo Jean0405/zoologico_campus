529f8942ce3d62cb8dca4e767e290c83e3a13918
jean0405 <jeanpierrea0405@gmail.com>
2023-08-22 13:34:22 -0500

    feat: :sparkles: inicialización del servidor
    Se inciializa el server junto con la config de sus variables de entorno

---
+++ Backend/src/app.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import express from "express";
+
+// Cargar variables de entorno
+dotenv.config();
+
+// Crear la aplicación Express
+const app = express();
+app.use(express.json());
+
+// Definir ruta raíz
+app.get("/", (req, res) => {
+  res.send("xdd");
+});
+
+// Obtener configuración del servidor desde variables de entorno
+const hostname = process.env.SERVER.HOSTNAME || "localhost";
+const port = process.env.SERVER.PORT || 3000;
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
