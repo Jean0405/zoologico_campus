4906790e22f6eb5f1fe5dac620daa2d754834e15
Jean0405 <jeanpierrea0405@gmail.com>
2023-08-24 23:03:19 -0500

    feat: :sparkles: Se agregan nuevos endpoints
    Se agregaron endpoints para las respectivas peticiones de ANIMALES, EMPLEADOS, ACTIVIDADES Y COMENTARIOS

--- Backend/src/v1/index.js
+++ Backend/src/v1/index.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import v1Animals from "./routes/animalesRoutes.js";
 import v1Actividades from "./routes/actividadesRoutes.js";
 import v1Empleados from "./routes/empleadosRoutes.js";
+import v1Comentarios from "./routes/comentariosRoutes.js";
 
 const v1Routes = Router();
 
 v1Routes
   .use("/animales", v1Animals)
   .use("/actividades", v1Actividades)
-  .use("/empleados", v1Empleados);
+  .use("/empleados", v1Empleados)
+  .use("/comentarios", v1Comentarios);
 
 export default v1Routes;
