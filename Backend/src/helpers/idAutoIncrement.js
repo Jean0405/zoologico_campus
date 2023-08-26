636ec577ff225908745b90ed22cff04b1295e703
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-26 17:41:11 -0500

    feat: :sparkles: Creacion Modulo IdAutoincrementable
    se creo una carpeta llamada helpers en la que tenemos un archivo llamado idAutoIncrement.js en el que creamos la funcion que dependiente el nombre de la coleccion qu le enviemos como parametro el nos creara la propiedad de ID autoincrementabe
    
    by:Carlos Villafrades

---
+++ Backend/src/helpers/idAutoIncrement.js
@@ -0,0 +1,12 @@
+import { connDB } from "../db/connectDB.js";
+
+export async function autoIncrementID(coleccion) {
+let db = await connDB();
+let collection = db.collection("counters");
+const sequenceDocument = await collection.findOneAndUpdate(
+{ ID: `${coleccion}Id` },
+{ $inc: { sequence_value: 1 } },
+{ returnDocument: "after" }
+);
+return sequenceDocument.value.sequence_value;
+}
