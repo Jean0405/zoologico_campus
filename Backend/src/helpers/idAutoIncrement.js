6ab351a317ee3d043d6cea79307db24910a69f1c
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-26 18:40:14 -0500

    feat: :card_file_box: creacion funcion autoincrementable en la db
    se creo una funcion autoincrementable directamente en el archivo de la base de datos por si se agregan datos directamente desde el archivo de la db
    
    by:Carlos Villafrades

--- Backend/src/helpers/idAutoIncrement.js
+++ Backend/src/helpers/idAutoIncrement.js
@@ -2,7 +2,7 @@ import { connDB } from "../db/connectDB.js";
 
 export async function autoIncrementID(coleccion) {
 let db = await connDB();
-let collection = db.collection("counters");
+let collection = db.collection("contadores");
 const sequenceDocument = await collection.findOneAndUpdate(
 { ID: `${coleccion}Id` },
 { $inc: { sequence_value: 1 } },
